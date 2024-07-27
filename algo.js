1bdb728ed0979ea5a3a73d5be93aec93430fd834
Megan Zehrt <zehrtmegan@gmail.com>
2024-07-27 14:44:25 -0500

    first commit

---
+++ algo.js
@@ -0,0 +1,25 @@
+// 2703. Return the Length of Arguments Passed
+
+
+
+// Write a function that returns the count of arguments passed to it.
+
+
+
+
+
+
+
+
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
